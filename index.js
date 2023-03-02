11b8a46b8a64ebd56025b62dd004baffbf0f504f
mdpw <38643182+mdpw@users.noreply.github.com>
2023-03-02 22:03:46 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 //create and populate a map at the same time
 const questionMap = new Map([
-['question','What is the ']
+  ['question', 'What is the best programming language?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'Javascript'],
+  ['answer', 3],
+  [true, 'Answer is correct'],
+  [false, 'Try Again'],
 ]);
+
+console.log(questionMap);
+
+console.log(questionMap.get('question'));
+for (const [key, value] of questionMap) {
+  if (typeof key === 'number') {
+    console.log(`${key}. ${value}`);
+  }
+}
+//const answer = prompt('Your answer')
